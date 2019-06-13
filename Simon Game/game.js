50a18cfcc9822f03e2282e162fc4210e0c538397
Huyen Pham <thanhhuyen2995@gmail.com>
2019-06-13 10:23:42 -0700

    Simon Game done

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -11,38 +11,71 @@ function playSound(name) {
 }
 
 function animatePress(currentColour) {
-  $("#"+currentColour).addClass("pressed");
+  $("#" + currentColour).addClass("pressed");
 
   setTimeout(function() {
-    $("#"+currentColour).removeClass("pressed");
-  },100);
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
 }
 
 function nextSequence() {
-  var randomNumber = Math.floor(3*Math.random());
+  userClickedPattern = [];
+  var randomNumber = Math.floor(3 * Math.random());
   var randomChosenColour = buttonColours[randomNumber];
-  level = level +1;
+  level = level + 1;
   $("#level-title").text("Level " + level);
   gamePattern.push(randomChosenColour);
 
-  $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+  $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   animatePress(randomChosenColour);
-  playSound("sounds/"+randomChosenColour+".mp3");
+  playSound("sounds/" + randomChosenColour + ".mp3");
 }
 
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   animatePress(userChosenColour);
-  playSound("sounds/"+userChosenColour+".mp3");
+  playSound("sounds/" + userChosenColour + ".mp3");
+
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 ///Game start
-
 $(document).keypress(function() {
-  if(started===false) {
+  if (started === false) {
     $("#level-title").text("Level " + level);
     nextSequence();
     started = true;
   }
 });
+
+//check the right answer
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      //5. Call nextSequence() to generate next pattern
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  //if the answer is wrong
+  else {
+    //display game over screen
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    //play game over sound
+    playSound("sounds/wrong.mp3");
+    startOver();
+  }
+}
+
+//start over the game
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
